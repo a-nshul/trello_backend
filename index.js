d4a48552a2641d4096c7e750d97120b7a23d6f43
a-nshul <upadhyay041298@gmail.com>
2024-11-11 19:00:17 +0530

    for production

--- index.js
+++ index.js
@@ -1,6 +1,5 @@
 const express = require("express");
 const cors = require("cors");
-const path = require("path");
 const dotenv = require("dotenv");
 const connectDB = require("./config/db");
 const userRoutes = require("./routes/userRoutes");
